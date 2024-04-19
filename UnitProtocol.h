78dffcbbcb97e4710d6cfc12cf2c904fbb2a2897
yanhaijun <15261882407@163.com>
2024-04-19 09:32:11 +0800

    no message

---
+++ UnitProtocol.h
@@ -0,0 +1,21 @@
+//
+//  UnitProtocol.h
+//  BaseKitHJY
+//
+//  Created by haijunyan on 2024/4/19.
+//
+
+#ifndef UnitProtocol_h
+#define UnitProtocol_h
+
+#import <Foundation/Foundation.h>
+
+@protocol UnitProtocol <NSObject>
+
+@optional
+//电话协议事件
+- (void)phoneCall;
+
+@end
+
+#endif /* UnitProtocol_h */
